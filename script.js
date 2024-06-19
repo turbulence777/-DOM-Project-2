aee11dec1104691039e5674875ba0c15afa05ff8
turbulence777 <joshuaakonyiro72@gmail.com>
2024-06-19 11:26:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const colorBox = document.getElementById('color-box');
+    const changeColorBtn = document.getElementById('change-color-btn');
+
+    function getRandomColor() {
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        return randomColor;
+    }
+
+    changeColorBtn.addEventListener('click', () => {
+        const newColor = getRandomColor();
+        colorBox.style.backgroundColor = newColor;
+    });
+});
